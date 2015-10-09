9727dc170f028efe62376269a3bb54b193f6289b
Marco Pivetta <ocramius@gmail.com>
2015-10-09 03:30:35 +0200

    Basic batch iterator coverage

---
+++ test/DoctrineBatchUtilsTest/BatchProcessing/SimpleBatchIteratorAggregateTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace DoctrineBatchUtilsTest\BatchProcessing;
+
+use Doctrine\Common\Persistence\Mapping\ClassMetadata;
+use Doctrine\ORM\AbstractQuery;
+use Doctrine\ORM\EntityManagerInterface;
+use DoctrineBatchUtils\BatchProcessing\SimpleBatchIteratorAggregate;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @covers \DoctrineBatchUtils\BatchProcessing\SimpleBatchIteratorAggregate
+ */
+final class SimpleBatchIteratorAggregateTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var AbstractQuery|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $query;
+
+    /**
+     * @var EntityManagerInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $entityManager;
+
+    /**
+     * @var ClassMetadata|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $metadata;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        $this->query         = $this->getMock(AbstractQuery::class, [], [], '', false);
+        $this->entityManager = $this->getMock(EntityManagerInterface::class);
+        $this->metadata      = $this->getMock(ClassMetadata::class);
+
+        $this->query->expects(self::any())->method('getEntityManager')->willReturn($this->entityManager);
+
+        parent::setUp();
+    }
+
+    /**
+     * @return void
+     */
+    public function testFromQuery()
+    {
+        $this->query->expects($this->any())->method('iterate')->will($this->returnValue(new \ArrayIterator()));
+
+        $this->assertInstanceOf(
+            SimpleBatchIteratorAggregate::class,
+            SimpleBatchIteratorAggregate::fromQuery($this->query, 100)
+        );
+    }
+}
+
