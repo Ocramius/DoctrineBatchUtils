7d26c13666026c8b810c1727348b46efaad11a3c
Marco Pivetta <ocramius@gmail.com>
2018-02-05 10:20:18 +0100

    Simplified batch-persist script

--- examples/persisting-new-objects-in-batch.php
+++ examples/persisting-new-objects-in-batch.php
@@ -1,59 +1,12 @@
 <?php
 
-use Doctrine\Common\Annotations\AnnotationReader;
-use Doctrine\Common\Annotations\AnnotationRegistry;
-use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\Mapping as ORM;
-use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
-use Doctrine\ORM\Tools\SchemaTool;
 use DoctrineBatchUtils\BatchProcessing\SimpleBatchIteratorAggregate;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-/**
- * @return EntityManager
- */
-$configureOrm = function () {
-    AnnotationRegistry::registerLoader('class_exists');
-    $configuration = new Configuration();
-
-    $configuration->setMetadataDriverImpl(new AnnotationDriver(new AnnotationReader(), [__DIR__]));
-    $configuration->setAutoGenerateProxyClasses(\Doctrine\ORM\Proxy\ProxyFactory::AUTOGENERATE_EVAL);
-    $configuration->setProxyNamespace('ORMProxies');
-    $configuration->setProxyDir(sys_get_temp_dir());
-
-    $entityManager = EntityManager::create(
-        [
-            'driverClass' => \Doctrine\DBAL\Driver\PDOSqlite\Driver::class,
-            'memory'      => true,
-        ],
-        $configuration
-    );
-
-    (new SchemaTool($entityManager))
-        ->createSchema(
-            $entityManager
-                ->getMetadataFactory()
-                ->getAllMetadata()
-        );
-
-    return $entityManager;
-};
-
-/** @ORM\Entity */
-class MyEntity
-{
-    /** @ORM\Id @ORM\GeneratedValue(strategy="NONE") @ORM\Column(type="integer") */
-    public $id;
-
-    public function __construct($id)
-    {
-        $this->id = $id;
-    }
-}
-
-$entityManager = $configureOrm();
+/** @var $entityManager EntityManager */
+$entityManager = call_user_func(require __DIR__ . '/bootstrap-orm.php');
 
 // Bootstrapping the ORM
 /** @var $iterable int[] */
