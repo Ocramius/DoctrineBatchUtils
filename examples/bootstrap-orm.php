425f20c4145b73f07d937e825af173d454d288f1
Marco Pivetta <ocramius@gmail.com>
2018-02-05 10:20:02 +0100

    Example script that bootstraps the ORM

---
+++ examples/bootstrap-orm.php
@@ -0,0 +1,41 @@
+<?php
+
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\ORM\Configuration;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Doctrine\ORM\Tools\SchemaTool;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/entity/MyEntity.php';
+
+/**
+ * @return EntityManager
+ */
+return function () {
+    AnnotationRegistry::registerLoader('class_exists');
+    $configuration = new Configuration();
+
+    $configuration->setMetadataDriverImpl(new AnnotationDriver(new AnnotationReader(), [__DIR__ . '/entity']));
+    $configuration->setAutoGenerateProxyClasses(\Doctrine\ORM\Proxy\ProxyFactory::AUTOGENERATE_EVAL);
+    $configuration->setProxyNamespace('ORMProxies');
+    $configuration->setProxyDir(sys_get_temp_dir());
+
+    $entityManager = EntityManager::create(
+        [
+            'driverClass' => \Doctrine\DBAL\Driver\PDOSqlite\Driver::class,
+            'memory'      => true,
+        ],
+        $configuration
+    );
+
+    (new SchemaTool($entityManager))
+        ->createSchema(
+            $entityManager
+                ->getMetadataFactory()
+                ->getAllMetadata()
+        );
+
+    return $entityManager;
+};
