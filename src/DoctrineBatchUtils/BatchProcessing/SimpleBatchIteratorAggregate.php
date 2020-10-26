c2fd57cf2888c19add8e26a143334ed92863cb63
azjezz <azjezz@protonmail.com>
2020-10-26 19:16:25 +0100

    fix static analysis build

--- src/DoctrineBatchUtils/BatchProcessing/SimpleBatchIteratorAggregate.php
+++ src/DoctrineBatchUtils/BatchProcessing/SimpleBatchIteratorAggregate.php
@@ -112,7 +112,9 @@ final class SimpleBatchIteratorAggregate implements IteratorAggregate
     private function reFetchObject(object $object): object
     {
         $metadata   = $this->entityManager->getClassMetadata(get_class($object));
-        $freshValue = $this->entityManager->find($metadata->getName(), $metadata->getIdentifierValues($object));
+        /** @psalm-var class-string $classname */
+        $classname = $metadata->getName();
+        $freshValue = $this->entityManager->find($classname, $metadata->getIdentifierValues($object));
 
         if (! $freshValue) {
             throw MissingBatchItemException::fromInvalidReference($metadata, $object);
