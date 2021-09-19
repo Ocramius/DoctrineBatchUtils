9e848eac24114620f2379a87169683b8c532fada
Marco Pivetta <ocramius@gmail.com>
2021-09-19 06:31:34 +0200

    Imported `ReturnTypeWillChange` class reference

--- src/DoctrineBatchUtils/BatchProcessing/SimpleBatchIteratorAggregate.php
+++ src/DoctrineBatchUtils/BatchProcessing/SimpleBatchIteratorAggregate.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\AbstractQuery;
 use Doctrine\ORM\EntityManagerInterface;
 use DoctrineBatchUtils\BatchProcessing\Exception\MissingBatchItemException;
 use IteratorAggregate;
+use ReturnTypeWillChange;
 use Throwable;
 use Traversable;
 
@@ -71,7 +72,7 @@ final class SimpleBatchIteratorAggregate implements IteratorAggregate
     /**
      * @return Traversable<TKey, TValue>
      */
-    #[\ReturnTypeWillChange]
+    #[ReturnTypeWillChange]
     public function getIterator(): iterable
     {
         $iteration = 0;
