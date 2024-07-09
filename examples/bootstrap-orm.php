31592b4a1af4f0124a6ad1239243a88917c4ca20
Indra Gunawan <hello@indra.my.id>
2024-07-09 19:14:16 +0800

    update major version dependencies

--- examples/bootstrap-orm.php
+++ examples/bootstrap-orm.php
@@ -1,7 +1,6 @@
 <?php
 
 use Doctrine\Common\Annotations\AnnotationReader;
-use Doctrine\Common\Annotations\AnnotationRegistry;
 use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
@@ -15,7 +14,6 @@ require_once __DIR__ . '/entity/MyEntity.php';
  * @return EntityManager
  */
 return function () {
-    AnnotationRegistry::registerLoader('class_exists');
     $configuration = new Configuration();
 
     $configuration->setMetadataDriverImpl(new AnnotationDriver(new AnnotationReader(), [__DIR__ . '/entity']));
