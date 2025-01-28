43629a552ed1b6227ffaf0598fc1675686009e59
Jérémy DECOOL <contact@jdecool.fr>
2025-01-28 20:53:26 +0100

    Fix PossiblyFalseArgument error in MissingBatchItemException

--- src/DoctrineBatchUtils/BatchProcessing/Exception/MissingBatchItemException.php
+++ src/DoctrineBatchUtils/BatchProcessing/Exception/MissingBatchItemException.php
@@ -19,12 +19,17 @@ class MissingBatchItemException extends UnexpectedValueException implements Exce
 {
     public static function fromInvalidReference(ClassMetadata $metadata, object $object): MissingBatchItemException
     {
+        $identifier = json_encode($metadata->getIdentifierValues($object));
+        if ($identifier === false) {
+            $identifier = 'unknown';
+        }
+
         return new self(sprintf(
             'Requested batch item %s#%s (of type %s) with identifier "%s" could not be found',
             get_class($object),
             spl_object_hash($object),
             $metadata->getName(),
-            json_encode($metadata->getIdentifierValues($object)),
+            $identifier,
         ));
     }
 }
